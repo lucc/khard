dd36cee21b3d97a51970f2556de35e08bf930e3c
Daniel Wilcox <dmw@yubasolutions.com>
2016-03-15 10:41:24 -0700

    Add compatibility scaffolding for moving toward python2+3.

--- khard/carddav_object.py
+++ khard/carddav_object.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
 # contact object class
 # vcard: https://tools.ietf.org/html/rfc6350
--- khard/khard.py
+++ khard/khard.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
 import tempfile, subprocess, os, sys, re, argparse, datetime
 from . import helpers
