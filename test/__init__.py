e7cb12ed79a471d95b06d6bd7877b32f1e6d653a
Lucas Hoffmann <l-m-h@web.de>
2016-07-19 09:17:02 +0200

    Add an empty test suite
    The test can be run with `python setup.py test` but no actual test cases where
    added yet.

---
+++ test/__init__.py
@@ -0,0 +1,10 @@
+"""Module to make it possible to import the test folder as a python package and
+hence make it possible to run all unittests from the top level direcotry with
+
+        python -m unittest [discover]
+
+and
+
+        python setup.py test
+
+"""
