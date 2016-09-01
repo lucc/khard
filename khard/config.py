c9a6330280e97bffd2100f7b5f321f448d24bbb8
Lucas Hoffmann <l-m-h@web.de>
2016-09-01 16:15:08 +0200

    Turn config.Config into a normal class
    The singleton interface is removed to make the class and usage less confusing.
    The singleton instance is replaced by a global variable in khard/khard.py.
    That should make it clear that modifications to this object are persistent (in
    contrast to modification to an object that was apearentrly generated on the
    fly and not saved: `Config().set_editor(string)`).  It also makes the point
    where the object is actually initialized much clearer.

--- khard/config.py
+++ khard/config.py
@@ -19,10 +19,6 @@ from . import helpers
 
 
 class Config:
-    """ A python singleton """
-
-    class __impl:
-        """ Implementation of the singleton interface """
 
         def __init__(self):
             self.config = None
@@ -495,25 +491,3 @@ class Config:
 
         def show_nicknames(self):
             return self.config['contact table']['show_nicknames']
-
-    ####################################
-    # storage for the instance reference
-    ####################################
-    __instance = None
-
-    def __init__(self):
-        """ Create singleton instance """
-        # Check whether we already have an instance
-        if Config.__instance is None:
-            # Create and remember instance
-            Config.__instance = Config.__impl()
-        # Store instance reference as the only member in the handle
-        self.__dict__['_Config__instance'] = Config.__instance
-
-    def __getattr__(self, attr):
-        """ Delegate access to implementation """
-        return getattr(self.__instance, attr)
-
-    def __setattr__(self, attr, value):
-        """ Delegate access to implementation """
-        return setattr(self.__instance, attr, value)
