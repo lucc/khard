6398b4696cecb62726034f5004874cca642e450b
Lucas Hoffmann <l-m-h@web.de>
2017-03-06 17:22:58 +0100

    Nicer formatting of string_to_date function

--- khard/helpers.py
+++ khard/helpers.py
@@ -75,32 +75,30 @@ def string_to_list(input, delimiter):
     return [x.strip() for x in input.split(delimiter)]
 
 
-def string_to_date(input):
-    """Convert string to date object.
+def string_to_date(string):
+    """Convert a date string into a date object.
 
-    :param input: the date string to parse
-    :type input: str
+    :param string: the date string to parse
+    :type string: str
     :returns: the parsed datetime object
     :rtype: datetime.datetime
     """
     # try date formats --mmdd, --mm-dd, yyyymmdd, yyyy-mm-dd and datetime
     # formats yyyymmddThhmmss, yyyy-mm-ddThh:mm:ss, yyyymmddThhmmssZ,
     # yyyy-mm-ddThh:mm:ssZ.
-    for format_string in ("--%m%d", "--%m-%d", "%Y%m%d", "%Y-%m-%d",
-                          "%Y%m%dT%H%M%S", "%Y-%m-%dT%H:%M:%S",
-                          "%Y%m%dT%H%M%SZ", "%Y-%m-%dT%H:%M:%SZ"):
+    for fmt in ("--%m%d", "--%m-%d", "%Y%m%d", "%Y-%m-%d", "%Y%m%dT%H%M%S",
+                "%Y-%m-%dT%H:%M:%S", "%Y%m%dT%H%M%SZ", "%Y-%m-%dT%H:%M:%SZ"):
         try:
-            return datetime.strptime(input, format_string)
+            return datetime.strptime(string, fmt)
         except ValueError:
-            pass
+            continue  # with the next format
     # try datetime formats yyyymmddThhmmsstz and yyyy-mm-ddThh:mm:sstz where tz
     # may look like -06:00.
-    for format_string in ("%Y%m%dT%H%M%S%z", "%Y-%m-%dT%H:%M:%S%z"):
+    for fmt in ("%Y%m%dT%H%M%S%z", "%Y-%m-%dT%H:%M:%S%z"):
         try:
-            return datetime.strptime(''.join(input.rsplit(":", 1)),
-                                     format_string)
+            return datetime.strptime(''.join(string.rsplit(":", 1)), fmt)
         except ValueError:
-            pass
+            continue  # with the next format
     raise ValueError
 
 
