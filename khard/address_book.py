e97d206564ecb5ae8dc7a7a60993de423637c2c8
Lucas Hoffmann <l-m-h@web.de>
2018-01-13 22:21:28 +0100

    Fix condition when looking for uids

--- khard/address_book.py
+++ khard/address_book.py
@@ -110,13 +110,14 @@ class AddressBook(metaclass=abc.ABCMeta):
         :rtype: generator(carddav_object.CarddavObject)
 
         """
-        contacts = []
+        found = False
         # Search for contacts with uid == query.
         for contact in self.contacts:
             if contact.get_uid() == query:
+                found = True
                 yield contact
         # If that fails, search for contacts where uid starts with query.
-        if not contacts:
+        if not found:
             for contact in self.contacts:
                 if contact.get_uid().startswith(query):
                     yield contact
