lucc/khard

commit 0f0ddd4d9385f06af611770afcc4e68b1ce05b32

Simplify recusive function

khard/helpers.py
@@ -41,10 +41,7 @@ def list_to_string(input, delimiter):
     converts list to string recursively so that nested lists are supported
     """
     if isinstance(input, list):
-        flat_list = []
-        for item in input:
-            flat_list.append(list_to_string(item, delimiter))
-        return delimiter.join(flat_list)
+        return delimiter.join(list_to_string(item) for item in input)
     return input
 
 

commit 836a94db533819e98da6e495a7b454bf47358c37

Fix regression in helpers.list_to_string()
The error was introduced in 0f0ddd4d9385f06af611770afcc4e68b1ce05b32.

khard/helpers.py
@@ -41,7 +41,8 @@ def list_to_string(input, delimiter):
     converts list to string recursively so that nested lists are supported
     """
     if isinstance(input, list):
-        return delimiter.join(list_to_string(item) for item in input)
+        return delimiter.join(
+            list_to_string(item, delimiter) for item in input)
     return input
 
 