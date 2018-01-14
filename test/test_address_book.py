86c9b307cd1a7a90ec1976f06ad39f497b99c833
Lucas Hoffmann <l-m-h@web.de>
2018-01-14 11:10:43 +0100

    Move tests after moving compare_uids()

--- test/test_address_book.py
+++ test/test_address_book.py
@@ -20,3 +20,27 @@ class AbstractAddressBookSearch(unittest.TestCase):
             with self.assertRaises(ValueError):
                 abook = _AddressBook('test', "/this-doesn't-exist")
                 abook.search('query', method='invalid_method')
+
+
+class AddressBookCompareUids(unittest.TestCase):
+
+    def test_different_strings(self):
+        uid1 = 'abc'
+        uid2 = 'xyz'
+        expected = 0
+        actual = address_book.AddressBook._compare_uids(uid1, uid2)
+        self.assertEqual(actual, expected)
+
+    def test_two_simple_strings(self):
+        uid1 = 'abcdef'
+        uid2 = 'abcxyz'
+        expected = 3
+        actual = address_book.AddressBook._compare_uids(uid1, uid2)
+        self.assertEqual(actual, expected)
+
+    def test_no_error_on_equal_strings(self):
+        uid = 'abcdefghij'
+        expected = len(uid)
+        actual = address_book.AddressBook._compare_uids(uid, uid)
+        self.assertEqual(actual, expected)
+
