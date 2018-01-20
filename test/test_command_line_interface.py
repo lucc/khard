303939a24dd86801e121bf5dcc24221e22cdcfe2
Lucas Hoffmann <l-m-h@web.de>
2018-01-20 11:40:39 +0100

    Abstract mocking of stdout into a function

--- test/test_command_line_interface.py
+++ test/test_command_line_interface.py
@@ -12,30 +12,34 @@ import unittest.mock as mock
 from khard import khard
 
 
+def mock_stdout():
+    stdout = io.StringIO()
+    context_manager = mock.patch('sys.stdout', stdout)
+    context_manager.getvalue = stdout.getvalue
+    return context_manager
+
+
 @mock.patch('sys.argv', ['TESTSUITE'])
 class HelpOption(unittest.TestCase):
 
     def test_global_help(self):
-        stdout = io.StringIO()
-        with mock.patch("sys.stdout", stdout):
-            with self.assertRaises(SystemExit):
+        with self.assertRaises(SystemExit):
+            with mock_stdout() as stdout:
                 khard.main(['-h'])
         text = stdout.getvalue().splitlines()
         self.assertTrue(text[0].startswith('usage: TESTSUITE [-h]'))
 
     @mock.patch.dict('os.environ', KHARD_CONFIG='test/fixture/minimal.conf')
     def test_subcommand_help(self):
-        stdout = io.StringIO()
-        with mock.patch("sys.stdout", stdout):
-            with self.assertRaises(SystemExit):
+        with self.assertRaises(SystemExit):
+            with mock_stdout() as stdout:
                 khard.main(['list', '-h'])
         text = stdout.getvalue().splitlines()
         self.assertTrue(text[0].startswith('usage: TESTSUITE list [-h]'))
 
     def test_global_help_with_subcommand(self):
-        stdout = io.StringIO()
-        with mock.patch("sys.stdout", stdout):
-            with self.assertRaises(SystemExit):
+        with self.assertRaises(SystemExit):
+            with mock_stdout() as stdout:
                 khard.main(['-h', 'list'])
         text = stdout.getvalue().splitlines()
         self.assertTrue(text[0].startswith('usage: TESTSUITE [-h]'))
@@ -45,8 +49,7 @@ class HelpOption(unittest.TestCase):
 class ListingCommands(unittest.TestCase):
 
     def test_simple_ls_without_options(self):
-        stdout = io.StringIO()
-        with mock.patch("sys.stdout", stdout):
+        with mock_stdout() as stdout:
             khard.main(['list'])
         text = [l.strip() for l in stdout.getvalue().splitlines()]
         expected = [
