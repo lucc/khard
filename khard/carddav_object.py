f7735ee4e7fe3760046647cd1548667f732e2eb1
Martin Stone <martin@d7415.co.uk>
2019-03-09 15:49:04 +0000

    Squashed commit of the following:
    commit fba0bc71a90ead708ca92f6c421679306419e26a
    Merge: e25906c 356fcea
    Author: Martin Stone <martin@d7415.co.uk>
    Date:   Sat Mar 9 14:28:35 2019 +0000
    
        Merge remote-tracking branch 'origin/develop' into pr_v2
    
    commit e25906c04698c622ad28caf1017420401555b556
    Author: Martin Stone <martin@d7415.co.uk>
    Date:   Mon Jan 28 23:37:46 2019 +0000
    
        Make more generic. Add YAML copmatibility. Add edit support.
    
    commit dc7fe64fbfec40eb89c58704bac2b47a0fadd1e9
    Author: Martin Stone <martin@d7415.co.uk>
    Date:   Tue Jan 15 23:42:02 2019 +0000
    
        Display ABLABELs with private objects and URLs

--- khard/carddav_object.py
+++ khard/carddav_object.py
@@ -121,6 +121,9 @@ class VCardWrapper:
         value.  This function returnes them untouched inside an agregating
         list.
 
+        If the property is part of a group containing exactly two items, with
+        exactly one ABLABEL. the property will be prefixed with that ABLABEL.
+
         :param name: the name of the property (should be UPPER case)
         :type name: str
         :returns: the values from all occurences of the named property
@@ -129,7 +132,8 @@ class VCardWrapper:
         values = []
         for child in self.vcard.getChildren():
             if child.name == name:
-                values.append(child.value)
+                ablabel = self._get_ablabel(child)
+                values.append(ablabel + (": " if ablabel else "") + child.value)
         return sorted(values)
 
     def _delete_vcard_object(self, name):
@@ -332,6 +336,53 @@ class VCardWrapper:
             except (AttributeError, ValueError):
                 pass
         return None
+
+    def _get_ablabel(self, item):
+        """Get an ABLABEL for a specified item in the vCard.
+        Will return the ABLABEL only if the item is part of a group with exactly
+        two items, exactly one of which is an ABLABEL.
+
+        :param item: the item to be labelled
+        :type item: vobject.base.ContentLine
+        :returns: the ABLABEL in the circumstances above or an empty string
+        :rtype: str
+
+        """
+        label = ""
+        if item.group:
+            count = 0
+            for child in self.vcard.getChildren():
+                if child.group and child.group == item.group:
+                    count += 1
+                    if child.name == "X-ABLABEL":
+                        if label == "":
+                            label = child.value
+                        else:
+                            return ""
+            if count != 2:
+                label = ""
+        return label
+
+    def _get_new_group(self, group_type=""):
+        """Get an unused group name for adding new groups. Uses the form item123
+         or itemgroup_type123 if a grouptype is specified.
+
+        :param group_type: (Optional) a string to add between "item" and the
+                           number
+        :type group_type: str
+        :returns: the name of the first unused group of the specified form
+        :rtype: str
+
+        """
+        counter = 1
+        while True:
+            group_name = "item%s%d" % (group_type, counter)
+            for child in self.vcard.getChildren():
+                if child.group and child.group ==  group_name:
+                    counter += 1
+                    break
+            else:
+                return group_name
 
     @anniversary.setter
     def anniversary(self, date):
@@ -567,14 +618,26 @@ class VCardWrapper:
     @property
     def webpages(self):
         """
-        :rtype: list(list(str))
+        :rtype: list(str)
         """
         return self._get_multi_property("URL")
 
     def _add_webpage(self, webpage):
         webpage_obj = self.vcard.add('url')
-        webpage_obj.value = convert_to_vcard("webpage", webpage,
-                                             ObjectType.string)
+        if isinstance(webpage, dict):
+            if len(webpage) > 1:
+                raise ValueError(
+                    "Error: webpage must be a string.")
+            label = [i for i in webpage][0]
+            group_name = self._get_new_group()
+            webpage_obj.group = group_name
+            webpage_obj.value = webpage[label]
+            ablabel_obj = self.vcard.add('X-ABLABEL')
+            ablabel_obj.group = group_name
+            ablabel_obj.value = label
+        else:
+            webpage_obj.value = helpers.convert_to_vcard("webpage", webpage,
+                                                         ObjectType.string)
 
     @property
     def categories(self):
@@ -1010,7 +1073,8 @@ class CarddavObject(VCardWrapper):
                     key = self.supported_private_objects[key_index]
                     if key not in private_objects:
                         private_objects[key] = []
-                    private_objects[key].append(child.value)
+                    ablabel = self._get_ablabel(child)
+                    private_objects[key].append(ablabel + (": " if ablabel else "") + child.value)
         # sort private object lists
         for value in private_objects.values():
             value.sort()
@@ -1018,7 +1082,20 @@ class CarddavObject(VCardWrapper):
 
     def _add_private_object(self, key, value):
         private_obj = self.vcard.add('X-' + key.upper())
-        private_obj.value = convert_to_vcard(key, value, ObjectType.string)
+        if isinstance(value, dict):
+            if len(value) > 1:
+                raise ValueError(
+                    "Error: " + key + " must be a string.")
+            label = [i for i in value][0]
+            group_name = self._get_new_group()
+            private_obj.group = group_name
+            private_obj.value = value[label]
+            ablabel_obj = self.vcard.add('X-ABLABEL')
+            ablabel_obj.group = group_name
+            ablabel_obj.value = label
+        else:
+            private_obj.value = helpers.convert_to_vcard(key, value,
+                                                         ObjectType.string)
 
     def get_formatted_anniversary(self):
         return self._format_date_object(self.anniversary, self.localize_dates)
