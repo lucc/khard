279a9044d0c0d2431ca9b69c2f779ea0ccbc7c91
Lucas Hoffmann <lucc@posteo.de>
2019-09-29 09:38:40 +0200

    Add tests for the actions module

---
+++ test/test_actions.py
@@ -0,0 +1,42 @@
+"""Tests for the action class"""
+
+import unittest
+
+from khard import actions
+
+action = 'list'
+alias = 'ls'
+unknown = 'this is not an action or an alias'
+
+
+class Action(unittest.TestCase):
+
+    def test_get_action_resolves_aliases(self):
+        self.assertEqual(action, actions.Actions.get_action(alias))
+
+    def test_get_action_returns_none_for_actions(self):
+        self.assertIsNone(actions.Actions.get_action(action))
+
+    def test_get_action_returns_none_for_unknown(self):
+        self.assertIsNone(actions.Actions.get_action(unknown))
+
+    def test_get_aliases_reverse_resolves_aliases(self):
+        self.assertEqual([alias], actions.Actions.get_aliases(action))
+
+    def test_get_aliases_returns_none_for_aliases(self):
+        self.assertIsNone(actions.Actions.get_aliases(alias))
+
+    def test_get_aliases_returns_none_for_unknown(self):
+        self.assertIsNone(actions.Actions.get_aliases(unknown))
+
+    def test_get_actions_returns_actions(self):
+        self.assertIn(action, actions.Actions.get_actions())
+
+    def test_get_actions_does_not_return_aliases(self):
+        self.assertNotIn(alias, actions.Actions.get_actions())
+
+    def test_get_all_returns_actions(self):
+        self.assertIn(action, actions.Actions.get_all())
+
+    def test_get_all_returns_aliases(self):
+        self.assertIn(alias, actions.Actions.get_all())
