cb090ea6b6be8770b90bbda4c7014c539b4f634f
Lucas Hoffmann <lucc@posteo.de>
2020-04-12 23:01:08 +0200

    Import all classes directly during query tests

--- test/test_query.py
+++ test/test_query.py
@@ -1,53 +1,52 @@
 import unittest
-from unittest import mock
 
-from khard import query
+from khard.query import AndQuery, AnyQuery, FieldQuery, NullQuery, OrQuery, TermQuery
 
 
 class TestTermQuery(unittest.TestCase):
 
     def test_match_if_query_is_anywhere_in_string(self):
-        q = query.TermQuery('bar')
+        q = TermQuery('bar')
         self.assertTrue(q.match('foo bar baz'))
 
     def test_query_terms_are_case_insensitive(self):
-        q = query.TermQuery('BAR')
+        q = TermQuery('BAR')
         self.assertTrue(q.match('foo bar baz'))
 
     def test_match_arguments_are_case_insensitive(self):
-        q = query.TermQuery('bar')
+        q = TermQuery('bar')
         self.assertTrue(q.match('FOO BAR BAZ'))
 
     def test_match_lists_by_mathing_recursive(self):
-        q = query.TermQuery('bar')
+        q = TermQuery('bar')
         self.assertTrue(q.match(['foo', 'bar baz']))
 
 
 class TestAndQuery(unittest.TestCase):
 
     def test_matches_if_all_subterms_match(self):
-        q1 = query.TermQuery("a")
-        q2 = query.TermQuery("b")
-        q = query.AndQuery(q1, q2)
+        q1 = TermQuery("a")
+        q2 = TermQuery("b")
+        q = AndQuery(q1, q2)
         self.assertTrue(q.match("ab"))
 
     def test_failes_if_at_least_one_subterm_fails(self):
-        q1 = query.TermQuery("a")
-        q2 = query.TermQuery("b")
-        q = query.AndQuery(q1, q2)
+        q1 = TermQuery("a")
+        q2 = TermQuery("b")
+        q = AndQuery(q1, q2)
         self.assertFalse(q.match("ac"))
 
 
 class TestOrQuery(unittest.TestCase):
 
     def test_matches_if_at_least_one_subterm_matchs(self):
-        q1 = query.TermQuery("a")
-        q2 = query.TermQuery("b")
-        q = query.OrQuery(q1, q2)
+        q1 = TermQuery("a")
+        q2 = TermQuery("b")
+        q = OrQuery(q1, q2)
         self.assertTrue(q.match("ac"))
 
     def test_failes_if_all_subterms_fail(self):
-        q1 = query.TermQuery("a")
-        q2 = query.TermQuery("b")
-        q = query.OrQuery(q1, q2)
+        q1 = TermQuery("a")
+        q2 = TermQuery("b")
+        q = OrQuery(q1, q2)
         self.assertFalse(q.match("cd"))
