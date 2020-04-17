1a3425004a7549eb0e83b1bb3e7839733fd0bf19
Lucas Hoffmann <lucc@posteo.de>
2020-04-17 23:06:06 +0200

    Fix type signature in formatter class

--- khard/formatter.py
+++ khard/formatter.py
@@ -1,6 +1,6 @@
 """Formatting and sorting of contacts"""
 
-from typing import List
+from typing import cast, Dict, List
 
 from .carddav_object import CarddavObject
 
@@ -25,28 +25,28 @@ class Formatter:
         self._show_nicknames = show_nicknames
 
     @staticmethod
-    def format_labeled_field(field, preferred):
+    def format_labeled_field(field: Dict[str, List[str]], preferred: List[str]
+                             ) -> str:
         """Format a labeled field from a vcard for display, the first entry
         under the preferred label will be returned
 
-        :param dict(str:list(str)) field: the labeled field
-        :param list(str) preferred: the order of preferred labels
+        :param field: the labeled field, this must not be empty!
+        :param preferred: the order of preferred labels
         :returns: the formatted field entry
-        :rtype: str
         """
         # filter out preferred type if set in config file
-        keys = []
+        found = []
         for pref in preferred:
             for key in field:
                 if pref.lower() in key.lower():
-                    keys.append(key)
-            if keys:
+                    found.append(key)
+            if found:
                 break
-        if not keys:
-            keys = [k for k in field if "pref" in k.lower()] or field.keys()
+        keys = found or [k for k in field if "pref" in k.lower()] \
+            or field.keys()
         # get first key in alphabetical order
         first_key = sorted(keys, key=lambda k: k.lower())[0]
-        return "{}: {}".format(first_key, sorted(field.get(first_key))[0])
+        return "{}: {}".format(first_key, sorted(field.get(first_key, []))[0])
 
     def get_special_field(self, vcard: CarddavObject, field: str) -> str:
         """Returns certain fields with specific formatting options
