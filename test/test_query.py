4ec09648016eb7ba7fae0cc1c08e25f5cd7bbdf9
Lucas Hoffmann <lucc@posteo.de>
2020-04-22 23:41:13 +0200

    Add tests for field queries

--- test/test_query.py
+++ test/test_query.py
@@ -1,6 +1,9 @@
 import unittest
 
-from khard.query import AndQuery, AnyQuery, FieldQuery, NullQuery, OrQuery, TermQuery
+from khard.query import AndQuery, AnyQuery, FieldQuery, NullQuery, OrQuery, \
+    TermQuery
+
+from .helpers import TestCarddavObject
 
 
 class TestTermQuery(unittest.TestCase):
@@ -73,3 +76,26 @@ class TestEquality(unittest.TestCase):
         first = AndQuery(null, any , term, field)
         second = AndQuery(any, null, field, term)
         self.assertEqual(first, second)
+
+
+class TestFieldQuery(unittest.TestCase):
+
+    def test_values_can_match_exact(self):
+        uid = 'Some Test Uid'
+        vcard = TestCarddavObject(uid=uid)
+        query = FieldQuery('uid', uid)
+        self.assertTrue(query.match(vcard))
+
+    def test_values_can_match_substrings(self):
+        uid = 'Some Test Uid'
+        vcard = TestCarddavObject(uid=uid)
+        query = FieldQuery('uid', 'e Test U')
+        self.assertTrue(query.match(vcard))
+
+    def test_valuess_can_match_case_insensitive(self):
+        uid = 'Some Test Uid'
+        vcard = TestCarddavObject(uid=uid)
+        query1 = FieldQuery('uid', uid.upper())
+        query2 = FieldQuery('uid', uid.lower())
+        self.assertTrue(query1.match(vcard))
+        self.assertTrue(query2.match(vcard))
