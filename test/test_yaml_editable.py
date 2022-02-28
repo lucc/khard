70b3a39cb55389a8ec8cba07baf42cc7263ea32b
Sebastian Kowalak <sebastian@skowalak.de>
2022-02-28 23:15:38 +0100

    add unit test for #276

--- test/test_yaml_editable.py
+++ test/test_yaml_editable.py
@@ -8,6 +8,22 @@ from khard.carddav_object import YAMLEditable
 from .helpers import TestYAMLEditable
 
 
+class ToYamlConversion(unittest.TestCase):
+
+    def test_yaml_quoted_special_characters(self):
+        yaml_editable = TestYAMLEditable()
+        yaml_editable.supported_private_objects = ["Twitter"]
+        yaml_repr = """
+Formatted name: Test vCard
+First name: Khard
+Private       :
+    Twitter: \"@khard\"
+"""
+        yaml_editable.update(yaml_repr)
+        yaml_dump = yaml_editable.to_yaml()
+        self.assertIn("'@khard'", yaml_dump)
+
+
 class ExceptionHandling(unittest.TestCase):
 
     def test_duplicate_key_errors_are_translated_to_value_errors(self):
