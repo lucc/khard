bb6c08a3b89ee5f25a0cfeee7955869f2529296b
Eric Scheibler <email@eric-scheibler.de>
2022-06-28 00:50:45 +0200

    Possible fix for #311: Filter email addresses by contact

--- khard/khard.py
+++ khard/khard.py
@@ -853,9 +853,10 @@ def email_subcommand(search_terms: Query, vcard_list: List[CarddavObject],
     formatter = Formatter(config.display, config.preferred_email_address_type,
                           config.preferred_phone_number_type,
                           config.show_nicknames, parsable)
-    all_emails = []
-    matched_emails = []
+    emails = []
     for vcard in vcard_list:
+        matched_emails = []
+        fallback_all_emails = []
         for type, email_list in sorted(vcard.emails.items(),
                                        key=lambda k: k[0].lower()):
             for email in sorted(email_list):
@@ -867,12 +868,13 @@ def email_subcommand(search_terms: Query, vcard_list: List[CarddavObject],
                     # else: start with name
                     fields = name, type, email
                 fields_serialized = "\t".join(fields)
-                all_emails.append(fields_serialized)
+                fallback_all_emails.append(fields_serialized)
                 if search_terms and search_terms.match(fields_serialized):
                     matched_emails.append(fields_serialized)
-    logger.debug("email addresses: all=%d, matched=%d", len(all_emails), len(matched_emails))
-    if all_emails:
-        emails = matched_emails if matched_emails else all_emails
+        logger.debug("email addresses for %s: all=%d, matched=%d",
+                str(vcard), len(fallback_all_emails), len(matched_emails))
+        emails += matched_emails if matched_emails else fallback_all_emails
+    if emails:
         if parsable:
             if not remove_first_line:
                 # at least mutt requires that line
