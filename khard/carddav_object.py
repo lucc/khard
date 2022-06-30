90a893e7fc3eca1ee708c0993b08983143452daf
Alexis Hildebrandt <afh@surryhill.net>
2022-06-30 16:11:18 +0200

    Add support for private kind on 3.0 contacts

--- khard/carddav_object.py
+++ khard/carddav_object.py
@@ -461,13 +461,16 @@ class VCardWrapper:
 
     @property
     def kind(self) -> str:
-        kind = self.get_first("kind", self._default_kind)
+        kind = self.get_first(self._kind_attribute_name().lower(), self._default_kind)
         return kind if kind != "org" else "organisation"
 
     @kind.setter
     def kind(self, value: str) -> None:
-        attr_prefix = "" if self.version == "4.0" else "X-"
-        self.vcard.add(f'{attr_prefix}KIND').value = value
+        value = value if value != "organisation" else "org"
+        self.vcard.add(self._kind_attribute_name()).value = value
+
+    def _kind_attribute_name(self) -> str:
+        return "{}KIND".format("" if self.version == "4.0" else "X-")
 
     @property
     def formatted_name(self) -> str:
@@ -1099,7 +1102,7 @@ class YAMLEditable(VCardWrapper):
                               contact_data)
 
         # kind
-        self._delete_vcard_object("KIND")
+        self._delete_vcard_object(self._kind_attribute_name())
         try:
             self.kind = contact_data["Kind"]
         except KeyError:
