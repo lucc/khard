7964eac4edd0e19e55d6735e87b3de13a07556a5
Lucas Hoffmann <lucc@posteo.de>
2023-03-19 13:00:07 +0100

    Fix version in flake for setuptools 66
    See https://setuptools.pypa.io/en/stable/history.html#v66-0-0.

--- flake.nix
+++ flake.nix
@@ -4,10 +4,9 @@
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.default =
-      nixpkgs.legacyPackages.x86_64-linux.khard.overrideAttrs (oa: rec {
-        pname = "khard";
+      nixpkgs.legacyPackages.x86_64-linux.khard.overridePythonAttrs (oa: rec {
         name = "khard-${version}";
-        version = "dev-${if self ? shortRev then self.shortRev else "dirty"}";
+        version = "${oa.version}post-dev+${if self ? shortRev then self.shortRev else "dirty"}";
         SETUPTOOLS_SCM_PRETEND_VERSION = version;
         src = ./.;
       });
