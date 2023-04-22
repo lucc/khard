c23e31387a7b6e803c1bd742b02fc4b89a3cf20e
Lucas Hoffmann <lucc@posteo.de>
2023-04-22 23:46:08 +0200

    Move most setuptools settings to pyproject.toml
    The "test_suite" key is the only one that I do not know how to convert.

--- setup.py
+++ setup.py
@@ -7,19 +7,4 @@
 
 from setuptools import setup
 
-with open('README.md', 'rb') as f:
-    readme = f.read().decode("utf-8")
-
-setup(
-    long_description=readme,
-    long_description_content_type='text/markdown',
-    extras_require={'doc': [
-        'sphinx',
-        'sphinx-autoapi',
-        'sphinx-autodoc-typehints'
-    ]},
-    packages=['khard', 'khard.helpers'],
-    package_data={'khard': ['data/*']},
-    test_suite="test",
-    include_package_data=True,
-)
+setup(test_suite="test")
