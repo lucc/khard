b864b4fb285ba6f07419a2b7bb240ca6ad3a7ac5
Lucas Hoffmann <lucc@posteo.de>
2023-05-01 23:15:55 +0200

    Fix data file packaging on nix

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
         name = "khard-${version}";
         version = "${oa.version}post-dev+${if self ? shortRev then self.shortRev else "dirty"}";
         SETUPTOOLS_SCM_PRETEND_VERSION = version;
+        postInstall = "cp -r $src/khard/data $out/lib/python*/site-packages/khard";
         src = ./.;
       });
     devShells.x86_64-linux.release =
