6ff616cdb5170453190a61df234ff586508aa982
Lucas Hoffmann <lucc@posteo.de>
2023-09-20 21:45:18 +0200

    Add tests for select helper function

--- test/test_helpers_interactive.py
+++ test/test_helpers_interactive.py
@@ -10,7 +10,7 @@ from .helpers import mock_stream
 
 class Select(unittest.TestCase):
 
-    def _test(expected, include_none=None):
+    def _test(self, include_none=None):
         input_list = ["a", "b", "c"]
         if include_none is None:
             return interactive.select(input_list)
@@ -18,7 +18,7 @@ class Select(unittest.TestCase):
             return interactive.select(input_list, include_none)
 
     def test_selection_index_is_1_based(self):
-        with mock.patch("builtins.input", lambda x: "1"):
+        with mock.patch("builtins.input", lambda _: "1"):
             actual = self._test()
         self.assertEqual(actual, "a")
 
@@ -39,6 +39,30 @@ class Select(unittest.TestCase):
         self.assertEqual(stdout, "Please enter an index value between 1 and 3 "
                          "or q to quit.\n")
         self.assertEqual(actual, "b")
+
+    def test_index_0_is_not_accepted(self):
+        with mock.patch("builtins.input", mock.Mock(side_effect=["0", "2"])):
+            with mock_stream() as stdout:
+                actual = self._test()
+        stdout = stdout.getvalue()
+        self.assertEqual(stdout, "Please enter an index value between 1 and 3 "
+                         "or q to quit.\n")
+        self.assertEqual(actual, "b")
+
+    def test_index_0_is_accepted_with_include_none(self):
+        with mock.patch("builtins.input", lambda _: "0"):
+            actual = self._test(True)
+        self.assertIsNone(actual)
+
+    def test_empty_input_cancels(self):
+        with mock.patch("builtins.input", lambda _: ""):
+            with mock_stream() as stdout:
+                actual = self._test()
+        stdout = stdout.getvalue()
+        self.assertEqual(stdout, "Canceled\n")
+        self.assertIsNone(actual)
+
+
 
 
 class Confirm(unittest.TestCase):
