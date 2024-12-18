973c2007422fb918441175107ba42f09cb224c6e
Lucas Hoffmann <lucc@posteo.de>
2024-12-19 00:28:59 +0100

    nix: add a default dev shell with more tools

--- flake.nix
+++ flake.nix
@@ -31,20 +31,18 @@
   };
   in {
     packages.${system}.default = pkgs.callPackage khard {};
-    devShells.${system}.release =
-      pkgs.mkShell {
-        packages = with pkgs; [
-          git
-          twine
-          (python3.withPackages (p: with p; [
-            build
-            mypy
-            pylint
-            setuptools
-            setuptools-scm
-            wheel
-          ] ++ self.packages.${system}.default.propagatedBuildInputs))
-        ];
+    devShells.${system} = let
+      khard = self.packages.${system}.default;
+      upstream = p: khard.nativeBuildInputs ++ khard.propagatedBuildInputs;
+      pythonEnv = pkgs.python3.withPackages (p: [
+        p.build
+        p.mypy
+        p.pylint
+      ] ++ (upstream p));
+    in {
+      default = pkgs.mkShell { packages = with pkgs; [ git pythonEnv ]; };
+      release = pkgs.mkShell {
+        packages = with pkgs; [ git twine pythonEnv ];
         shellHook = ''
           cat <<EOF
           To publish a tag on pypi
@@ -57,6 +55,7 @@
           EOF
         '';
       };
+    };
     checks.${system} = {
       tests-python-311 = pkgs.callPackage khard { python3 = pkgs.python311; };
       tests-python-312 = pkgs.callPackage khard { python3 = pkgs.python312; };
