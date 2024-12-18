555ce4848d56598c342c4546bca967839d1e8705
Lucas Hoffmann <lucc@posteo.de>
2024-12-19 00:31:58 +0100

    Format nix code

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   description = "Development flake for khard";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     inherit (builtins) map head split replaceStrings mapAttrs;
@@ -12,41 +15,49 @@
     deps = map clean pyproject.project.dependencies;
     opts = mapAttrs (name: map clean) pyproject.project.optional-dependencies;
     get = names: pkgs: map (name: pkgs.${name}) names;
-    khard = { python3, doc ? true }: python3.pkgs.buildPythonApplication {
-      pname = "khard";
-      version = "0.dev+${self.shortRev or self.dirtyShortRev}";
-      pyproject = true;
-      src = ./.;
-      nativeBuildInputs = let
-        names = build
-          ++ lists.optionals doc opts.doc
-          ++ lists.optional doc "sphinxHook";
-      in get names python3.pkgs;
-      sphinxBuilders = [ "man" ];
-      propagatedBuildInputs = get deps python3.pkgs;
-      postInstall = ''
-        install -D misc/zsh/_khard $out/share/zsh/site-functions/_khard
-        cp -r $src/khard/data $out/lib/python*/site-packages/khard
-      '';
-      # see https://github.com/scheibler/khard/issues/263
-      preCheck = "export COLUMNS=80";
-      pythonImportsCheck = [ "khard" ];
-      checkPhase = "python3 -W error -m unittest -v";
-  };
+    khard = {
+      python3,
+      doc ? true,
+    }:
+      python3.pkgs.buildPythonApplication {
+        pname = "khard";
+        version = "0.dev+${self.shortRev or self.dirtyShortRev}";
+        pyproject = true;
+        src = ./.;
+        nativeBuildInputs = let
+          names =
+            build
+            ++ lists.optionals doc opts.doc
+            ++ lists.optional doc "sphinxHook";
+        in
+          get names python3.pkgs;
+        sphinxBuilders = ["man"];
+        propagatedBuildInputs = get deps python3.pkgs;
+        postInstall = ''
+          install -D misc/zsh/_khard $out/share/zsh/site-functions/_khard
+          cp -r $src/khard/data $out/lib/python*/site-packages/khard
+        '';
+        # see https://github.com/scheibler/khard/issues/263
+        preCheck = "export COLUMNS=80";
+        pythonImportsCheck = ["khard"];
+        checkPhase = "python3 -W error -m unittest -v";
+      };
   in {
     packages.${system}.default = pkgs.callPackage khard {};
     devShells.${system} = let
       khard = self.packages.${system}.default;
       upstream = p: khard.nativeBuildInputs ++ khard.propagatedBuildInputs;
-      pythonEnv = pkgs.python3.withPackages (p: [
-        p.build
-        p.mypy
-        p.pylint
-      ] ++ (upstream p));
+      pythonEnv = pkgs.python3.withPackages (p:
+        [
+          p.build
+          p.mypy
+          p.pylint
+        ]
+        ++ (upstream p));
     in {
-      default = pkgs.mkShell { packages = with pkgs; [ git pythonEnv ]; };
+      default = pkgs.mkShell {packages = with pkgs; [git pythonEnv];};
       release = pkgs.mkShell {
-        packages = with pkgs; [ git twine pythonEnv ];
+        packages = with pkgs; [git twine pythonEnv];
         shellHook = ''
           cat <<EOF
           To publish a tag on pypi
@@ -61,8 +72,8 @@
       };
     };
     checks.${system} = {
-      tests-python-311 = pkgs.callPackage khard { python3 = pkgs.python311; };
-      tests-python-312 = pkgs.callPackage khard { python3 = pkgs.python312; };
+      tests-python-311 = pkgs.callPackage khard {python3 = pkgs.python311;};
+      tests-python-312 = pkgs.callPackage khard {python3 = pkgs.python312;};
     };
   };
 }
