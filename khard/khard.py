a40d94f516edf260f5c5e1267a20caeb9e7faceb
Lucas Hoffmann <lucc@posteo.de>
2025-03-13 22:46:12 +0100

    Remove unneeded list from comprehension

--- khard/khard.py
+++ khard/khard.py
@@ -218,7 +218,7 @@ def list_contacts(vcard_list: list[Contact], fields: Iterable[str] = (),
             else:
                 row.append(formatter.get_nested_field(vcard, field))
         if parsable:
-            print("\t".join([str(v) for v in row]))
+            print("\t".join(str(v) for v in row))
         else:
             table.append(row)
     if not parsable:
